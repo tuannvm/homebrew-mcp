6591b1422107f9034c20ee53fe9a1b94adce443e
GitHub Action <action@github.com>
2025-04-25 02:35:32 +0000

    Update haproxy-mcp-server.rb to version 1.0.10

--- haproxy-mcp-server.rb
+++ haproxy-mcp-server.rb
@@ -1,28 +1,28 @@
   class HaproxyMcpServer < Formula
     desc "haproxy-mcp-server - Add description here"
     homepage "https://github.com/tuannvm/haproxy-mcp-server"
-    version "1.0.0"
+    version "1.0.10"
     license "Apache-2.0"
   
     on_macos do
       on_arm do
         url "https://github.com/tuannvm/haproxy-mcp-server/releases/download/v1.0.0/haproxy-mcp-server_1.0.0_darwin_arm64.tar.gz"
-        sha256 "92dd0a14cb9705d38019554c1c8c3417420d67cf7a5e50121431c230ce2c3efa"
+        sha256 "200a25d232f86f84e4f152507d7215c6fc2873e88bafdf39e93152138d004d93"
       end
       on_intel do
         url "https://github.com/tuannvm/haproxy-mcp-server/releases/download/v1.0.0/haproxy-mcp-server_1.0.0_darwin_amd64.tar.gz"
-        sha256 "ee8f7cb99cfb9636aa5a8eb9d1d84b140fa547bb97c7b1de90218c1344c4c258"
+        sha256 "14e96b414921af277b45347f335489a1536fa434e0dd338636b520a49db7f1b5"
       end
     end
   
     on_linux do
       on_arm do
         url "https://github.com/tuannvm/haproxy-mcp-server/releases/download/v1.0.0/haproxy-mcp-server_1.0.0_linux_arm64.tar.gz"
-        sha256 "07fd3282c84a7dedce28f2060c94bb7157c53cfd3129d82d0bd73152b51995ad"
+        sha256 "316488eade592bbe6ca1fa8bb81adcd8756cdcde90c030f6a3636fc86c4392dd"
       end
       on_intel do
         url "https://github.com/tuannvm/haproxy-mcp-server/releases/download/v1.0.0/haproxy-mcp-server_1.0.0_linux_amd64.tar.gz"
-        sha256 "ec18aa69ed2487a7958e73da2b0ddc9b26de5955af02e49d223a859a5e0e9704"
+        sha256 "342ea916d29861595a45fdde19874e315b97c0d3f0f59e79e9e80a5988d92481"
       end
     end
   
