95b97fafdee88080c672df90c7d5d28dd74849fa
GitHub Action <action@github.com>
2025-05-03 06:47:03 +0000

    Update kafka-mcp-server.rb to version 1.0.3

--- kafka-mcp-server.rb
+++ kafka-mcp-server.rb
@@ -1,28 +1,28 @@
   class KafkaMcpServer < Formula
     desc "kafka-mcp-server - Add description here"
     homepage "https://github.com/tuannvm/kafka-mcp-server"
-    version "1.0.2"
+    version "1.0.3"
     license "Apache-2.0"
   
     on_macos do
       on_arm do
         url "https://github.com/tuannvm/kafka-mcp-server/releases/download/v1.0.0/kafka-mcp-server_1.0.0_darwin_arm64.tar.gz"
-        sha256 "e24f047af2f9f33e7f5f6b4b656bbfd1a375140d7ca999619d53a06d1997f1af"
+        sha256 "3c4d19889f136e5554dc15c7e86846f88b112aefce5654fdc291b1baffd5a3ea"
       end
       on_intel do
         url "https://github.com/tuannvm/kafka-mcp-server/releases/download/v1.0.0/kafka-mcp-server_1.0.0_darwin_amd64.tar.gz"
-        sha256 "826f32d9a0bbf197d500cc0712e7ec78047da7fef7c0b220b8051f6a44545b50"
+        sha256 "fa2714ec9d4a81e6f20f729b40390f3543675c180eca623f8f888cece2b2ea5d"
       end
     end
   
     on_linux do
       on_arm do
         url "https://github.com/tuannvm/kafka-mcp-server/releases/download/v1.0.0/kafka-mcp-server_1.0.0_linux_arm64.tar.gz"
-        sha256 "a5b2d98ed6cf3b0b4575ec79009cd42e818d649b301fae2c028e10156b3c9afd"
+        sha256 "751a6aaec9fb09ccef884a9845a2b1275fd6cda87cb104ef79c2bf7fc2d3cb11"
       end
       on_intel do
         url "https://github.com/tuannvm/kafka-mcp-server/releases/download/v1.0.0/kafka-mcp-server_1.0.0_linux_amd64.tar.gz"
-        sha256 "81e66ed1d1ee99383b4878ea29eca9faef7363da1f1d049b6d974dced6d8f467"
+        sha256 "91855cc129fa07a550db706c0cabe485c9cef892f767cc03935a7f42e0af8237"
       end
     end
   
