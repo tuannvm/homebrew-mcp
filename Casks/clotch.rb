14c4701b9b42f4425c271c4216998503a436074a
Tommy Nguyen <tuannvm@hotmail.com>
2026-04-14 08:17:43 -0700

    Update clotch cask SHA for notarized v0.2.0
    Signed-off-by: Tommy Nguyen <tuannvm@hotmail.com>

--- Casks/clotch.rb
+++ Casks/clotch.rb
@@ -1,6 +1,6 @@
 cask "clotch" do
   version "0.2.0"
-  sha256 "f5fd51bb9acad501bcac4aac1f1e3f7732643dd1109fa7e9f28242e56fa22e5c"
+  sha256 "8d7c4088eedb700cb32f397c2f6a30e982b20ec0e5af34ba648d58509b637efb"
 
   url "https://github.com/tuannvm/clotch/releases/download/v0.2.0/Clotch-v0.2.0-darwin-arm64.zip"
   name "Clotch"
