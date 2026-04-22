c61ad14a4cf0b92f0fedbb03469090438809fc90
Tommy Nguyen <tuannvm@hotmail.com>
2026-04-22 10:18:30 -0700

    Update clotch cask to v0.2.18
    Signed-off-by: Tommy Nguyen <tuannvm@hotmail.com>

--- Casks/clotch.rb
+++ Casks/clotch.rb
@@ -1,8 +1,8 @@
 cask "clotch" do
-  version "0.2.16"
-  sha256 "7aac83254399dc18ad032d98fdc1ac925f046e9762258994ef079f606ee34b67"
+  version "0.2.18"
+  sha256 "6ab9ce278ad59a06ae69c2353283da669352043193303dcee71f105b831d7040"
 
-  url "https://github.com/tuannvm/clotch/releases/download/v0.2.16/Clotch-v0.2.16-darwin-arm64.zip"
+  url "https://github.com/tuannvm/clotch/releases/download/v0.2.18/Clotch-v0.2.18-darwin-arm64.zip"
   name "Clotch"
   desc "Dynamic Island monitor for Claude Code sessions"
   homepage "https://github.com/tuannvm/clotch"
